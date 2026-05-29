e194d632f7b0f25eaca51e708433c8aa0a2be7e8
.trellis <trellis@localhost>
2026-05-29 01:32:21 -0400

    supervisor2 checkpoint 000071 | cycle 11 | TheoremStating | Start | ProductDigraphForwardIndependentBound

--- Tablet/ProductDigraphForwardIndependentBound.lean
+++ Tablet/ProductDigraphForwardIndependentBound.lean
@@ -12,14 +12,15 @@ universe u
 
 theorem ProductDigraphForwardIndependentBound {V : Type u} [Fintype V]
     (F G : LoopGraph V) (s n dF dG : ℕ) (lambdaF lambdaG eta w : ℝ)
-    (hs : 3 ≤ s) (hn : 3 ≤ n)
+    (hs : 3 ≤ s) (hn : 3 ≤ n) (hdF : 0 < dF) (hdG : 0 < dG)
     (hF : LoopGraphNdLambda F n dF lambdaF)
     (hG : LoopGraphNdLambda G n dG lambdaG)
     (hFG : HsFreePair F G s)
     (heta : eta =
       max (lambdaG ^ 2 / (dG : ℝ) ^ 2)
         (lambdaF * lambdaG / ((dF : ℝ) * (dG : ℝ))))
-    (hw : w = 4 * (n : ℝ) * Real.log (n : ℝ) / (dG : ℝ)) :
+    (hw : w = 4 * (n : ℝ) * Real.log (n : ℝ) / (dG : ℝ))
+    (heta_nonneg : 0 ≤ eta) (heta_le_one : eta ≤ 1) :
     ∃ (W : Type u) (_ : Fintype W), ∃ D : Digraph W,
       TransitiveTournamentFree D s ∧
         Fintype.card W = dF * n ∧
